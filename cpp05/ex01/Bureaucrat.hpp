7fc4bf14a1ab8635de747669df2502eff8b173fb
edkeller <eduardokeller00@gmail.com>
2025-12-01 18:43:24 -0300

    m05/finish ex00

--- cpp05/ex01/Bureaucrat.hpp
+++ cpp05/ex01/Bureaucrat.hpp
@@ -6,7 +6,7 @@
 /*   By: ekeller-@student.42sp.org.br <ekeller-@    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/28 13:04:40 by ekeller-@st       #+#    #+#             */
-/*   Updated: 2025/12/01 18:03:00 by ekeller-@st      ###   ########.fr       */
+/*   Updated: 2025/12/01 18:17:07 by ekeller-@st      ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -30,12 +30,12 @@ public:
 	~Bureaucrat();
 
 	const std::string& getName() const;
-	// //for primitive types such as int is better to return
-	// //a copy(by value). not a reference. is more efficient.
 	const int		getGrade() const;
 	void			incrementGrade();
 	void			decrementGrade();
 	void			setGrade(int grade);
+
+	void 			signForm(Form &form);
 
 	class GradeTooHighException: public std::exception {
 		public:
